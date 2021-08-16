859a0d8a4156babf2e55abfa4624a78423da0f42
NicoleYOsborn <nicole.y.osborn@gmail.com>
2021-08-16 13:31:21 -0700

    total score

---
+++ pinbrain/src/components/TotalScore/TotalScore.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './totalScore.css';
+
+function TotalScore(props) {
+
+    return (
+        <table className="totalScore" >
+            <tr>
+                <th>Total Score</th>
+            </tr>
+            <tr className="frametotals">
+                <td>
+                    <div></div>
+                </td>
+            </tr>
+        </table>
+    )
+}
+
+export default TotalScore;
---
+++ pinbrain/src/components/TotalScore/TotalScore.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TotalScore from './TotalScore';
+
+export default {
+    title: 'Components/TotalScore',
+    component: TotalScore
+};
+
+export const Total = () => <TotalScore />
